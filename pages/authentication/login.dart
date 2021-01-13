20c62c52dd9a4de32df8bceef3e445a12c73ae83
alreadyknowit <47862902+alreadyknowit@users.noreply.github.com>
2021-01-14 00:42:15 +0300

    register database handled

---
+++ pages/authentication/login.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/widgets/first.dart';
+import 'package:chat_app/widgets/forgot.dart';
+import 'package:chat_app/widgets/textLogin.dart';
+import 'package:chat_app/widgets/verticalText.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [Color(0xff2F0743), Color(0xff41295a)]),
+        ),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Row(children: <Widget>[
+                  VerticalText(),
+                  TextLogin(),
+                ]),
+                Padding(
+                  padding: const EdgeInsets.only(top: 50, left: 50, right: 50),
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color:Colors.green),
+                        ) ,
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.cyan),
+                        ),
+                        fillColor: Colors.lightBlueAccent,
+                        labelText: 'Username',
+                        labelStyle: TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 50, right: 50),
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Forgot(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, right: 50, left: 200),
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue[300],
+                          blurRadius:
+                              10.0, // has the effect of softening the shadow
+                          spreadRadius:
+                              1.0, // has the effect of extending the shadow
+                          offset: Offset(
+                            5.0, // horizontal, move right 10
+                            5.0, // vertical, move down 10
+                          ),
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'OK',
+                            style: TextStyle(
+                              color: Colors.lightBlueAccent,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                FirstTime(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
